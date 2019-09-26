a7d16ebbec7e00c0d8b457334d0b404fdb7ae758
Hidayatullah Agung Prasetyo <hidayatullahap@gmail.com>
2019-09-27 00:08:04 +0700

    add crate div

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // https://stackoverflow.com/questions/6789703/how-to-write-javascript-in-client-side-to-receive-and-parse-chunked-response-i
 fetch("http://localhost:1323").then(function (response) {
     console.log('Stream start');
+    createDiv('Stream start');
     let reader = response.body.getReader();
     let decoder = new TextDecoder();
 
@@ -10,12 +11,21 @@ fetch("http://localhost:1323").then(function (response) {
         return reader.read().then(function ({ value, done }) {
             if (done) {
                 console.log('Stream complete');
+                createDiv('Stream complete');
                 return;
             }
 
             let newData = decoder.decode(value, { stream: !done });
             console.log(newData);
+            createDiv(newData);
             return readData();
         });
     }
 });
+
+function createDiv(message) {
+    var div = document.createElement("div");
+    div.innerHTML = message;
+
+    document.getElementById("app").appendChild(div);
+}
